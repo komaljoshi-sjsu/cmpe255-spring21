e00e068f3941cfa92bf2be01241395fe0c38f03a
Komal Joshi <komal.joshi@sjsu.edu>
2021-03-11 16:15:30 -0800

    contains linear and polynomial regression of degree 2 and 20

--- assignment1/linearReg.py
+++ assignment1/linearReg.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
@@ -27,6 +28,10 @@ class Linear_Ref:
         self.df_target_test = self.df_target.iloc[self.n_train:].copy()
 
         self.reg = LinearRegression()
+    
+    def initPolyRegDegree(self,deg):
+        self.poly_lin = LinearRegression()
+        self.poly = PolynomialFeatures(degree=deg)
 
     def split_train_test(self,data):
         df_train = data.iloc[:self.n_train].copy()
@@ -35,6 +40,10 @@ class Linear_Ref:
 
     def linear_reg(self,df_train):
         self.reg.fit(df_train,self.df_target_train)
+
+    def poly_reg(self,x):
+        df_train_poly_form = self.poly.fit_transform(x)
+        self.poly_lin.fit(df_train_poly_form,self.df_target_train.values)
 
     def predict(self,df_test):
         return pd.DataFrame(self.reg.predict(df_test))
@@ -47,17 +56,20 @@ class Linear_Ref:
         return r2_score(self.df_target_test, df_predict,multioutput='variance_weighted')
 
 
-    def plot(self):
-        print('Most optimum col is: '+self.final_feature_name)
-        print('R-Squared Value is: '+str(self.final_r2))
-        print('RMSE Value is: '+str(self.final_rmse))
-        plt.scatter(self.final_df_test,self.final_df_predict)
-        plt.title("Scatter Plot of test data and predicted value")
-        plt.xlabel(self.final_feature_name)
+    def plot(self,final_rmse,final_r2,final_df_predict,final_df_test,final_feature_name,reg_type):
+        print('Plotting for '+reg_type)
+        print('Most optimum col is: '+final_feature_name)
+        print('R-Squared Value is: '+str(final_r2))
+        print('RMSE Value is: '+str(final_rmse))
+        plt.figure(figsize=(8,6)) #set before plotting
+        plt.scatter(final_df_test,self.df_target_test,color='blue')
+        plt.plot(final_df_test,final_df_predict,color='red')
+        plt.title("Scatter Plot of test data and predicted value - "+reg_type)
+        plt.xlabel(final_feature_name)
         plt.ylabel("MEDV")
         plt.show()
 
-    def start_reg(self) :
+    def start_lin_reg(self) :
         for col in self.df_data.columns.values :
             df_x = self.df_data[col].copy().to_frame(col)
 
@@ -74,9 +86,27 @@ class Linear_Ref:
                 self.final_df_predict = df_y_predict.values
                 self.final_feature_name = col
                 self.final_rmse =  rmseVal
-        self.plot()
+        self.plot(self.final_rmse,self.final_r2,self.final_df_predict,self.final_df_test,self.final_feature_name,'Linear Regression')
 
+    def start_poly_reg(self,reg_type):
+        df_x = (self.df_data[self.final_feature_name].copy()).to_frame(self.final_feature_name)
+        df_x_train, df_x_test = self.split_train_test(df_x)
+
+        self.poly_reg(df_x_train.values)
+        
+        x_poly = self.poly.fit_transform(df_x_test.values)
+        y_predict = self.poly_lin.predict(x_poly)
+
+        df_y_predict = pd.DataFrame(y_predict)
+        rmseVal = self.rmse(df_y_predict)
+        r2 = self.r_sq_score(df_y_predict)
+
+        self.plot(rmseVal,r2,y_predict,df_x_test.values,self.final_feature_name,reg_type)
 if __name__ == '__main__':
     lr = Linear_Ref()
-    lr.start_reg()
+    lr.start_lin_reg()
+    lr.initPolyRegDegree(2)
+    lr.start_poly_reg('Polynomial Regression - Degree 2')
+    lr.initPolyRegDegree(20)
+    lr.start_poly_reg('Polynomial Regression - Degree 20')
 
