e0c1f0a71e8d569ca81e3b5cdd6c52d98269de38
Komal Joshi <komal.joshi@sjsu.edu>
2021-03-15 13:57:51 -0700

    image classification assignment

---
+++ lab3/Readme.md
@@ -0,0 +1,24 @@
+# CMPE 255 LAB 3
+
+Try to build a number-5 detector binary classifier for the MNIST dataset from Scikit-Learn.
+
+Use lab3.py as baseline and How to load MINIST Dataset
+
+## Approach
+
+Sklearn's fetch_openml() is used to load mnist dataset. Since fetch_openml() gives unsorted data so it is sorted based on target values so that we can easily test our data.
+
+Then, we select a random digit whose classification we will predict whether it is 5 or not. 
+
+We now train our data for values of X. Note that we take y_train_5 value as y_train == 5. This is because this statement will give us series of booleans which show whether given entry is 5 or not. We train our data using this X,y_train_5 dataset.
+
+Once trained, we pass the random digit we calculated in previous step and pass it to our classifier to see whether it is detecting the result properly or not. We also find the cross value score.
+
+We can pass random digits from test set of our data set and verify the claasification result. Cross validation score will showcase accuracy of the model.
+
+## Result
+
+The following image shows actual digit value and its corresponding result whether it is 5 or not. Image is not shown in results, however, when running python script, first image will be shown and then its computation will get executed.
+
+![picture](images/lab3.png)
+
---
+++ lab3/lab3.py
@@ -0,0 +1,90 @@
+import numpy as np
+import os
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import SGDClassifier
+np.random.seed(42)
+
+# To plot pretty figures
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+PROJECT_ROOT_DIR = "."
+IMAGE_DIR = "images"
+
+X=None
+y=None
+sgd_clf=None
+
+def sort_by_target(mnist):
+    reorder_index = np.array(sorted([(target, i) for i, target in enumerate(mnist.target)]))[:, 1]
+    mnist.data = mnist.data.iloc[reorder_index]
+    mnist.target= mnist.target[reorder_index]
+
+def load_and_sort():
+    global X
+    global y
+    mnist = fetch_openml('mnist_784', version=1,cache=True)
+    mnist.target = mnist.target.astype(np.int8)
+    sort_by_target(mnist)
+    X,y = mnist["data"],mnist["target"]
+    mnist["data"], mnist["target"]
+
+
+def random_digit(x,i):
+    some_digit = x.iloc[[i]] #taken ith entry of dataframe
+    some_digit_image = some_digit.values.reshape(28, 28)
+    plt.imshow(some_digit_image, cmap = mpl.cm.binary,
+            interpolation="nearest")
+    plt.axis("off")
+
+    #save_fig("some_digit_plot")
+    plt.show()
+    return some_digit
+
+def calculate_cross_val_score(x,y):
+    score = cross_val_score(sgd_clf,x,y)
+    print(f'cross validation score is: {score}')
+
+def train_predict(some_digit,index):
+    global sgd_clf
+    shuffle_index = np.random.permutation(60000)
+    X_train, y_train = X.iloc[:60000], y[:60000]
+
+    X_test, y_test = X.iloc[60000:], y[60000:]
+
+    X_train, y_train = X_train.iloc[shuffle_index],y_train.iloc[shuffle_index]
+    # Binary number 5 Classifier
+    y_train_5 = (y_train == 5)
+    y_test_5 = (y_test == 5)
+
+    
+    # print prediction result of the given input some_digit
+    sgd_clf = SGDClassifier(max_iter=20,tol=-np.infty)
+    sgd_clf.fit(X_train, y_train_5)
+
+    prediction = sgd_clf.predict(some_digit)
+    print(f'\n\nActual digit value = {y.iloc[index]} \n Is this digit 5?  {prediction}')
+    calculate_cross_val_score(X_train,y_train_5)
+    
+    return X_train,y_train,y_train_5,X_test, y_test_5, y_test
+
+def test() :
+    load_and_sort()
+    somedigit = random_digit(X,36000)
+    X_train,y_train,y_train_5,X_test, y_test_5, y_test = train_predict(somedigit,36000)
+
+    somedigit = random_digit(X_train,15000)
+    prediction = sgd_clf.predict(somedigit)
+    print(f'\n\nActual digit value = {y_train.iloc[15000]} \n Is this digit 5?  {prediction}')
+    calculate_cross_val_score(X_train,y_train_5)
+
+    somedigit = random_digit(X_train,36500)
+    prediction = sgd_clf.predict(somedigit)
+    print(f'\n\nActual digit value = {y_train.iloc[36500]} \n Is this digit 5?  {prediction}')
+    calculate_cross_val_score(X_train,y_train_5)
+
+test()
